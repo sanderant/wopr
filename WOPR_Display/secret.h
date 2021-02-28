8e8f134c8c7978c4019d3c3046aa740d79fa57a4
Alexander Antoniades <sander@antoniades.com>
2021-02-28 18:45:46 -0500

    Also update secret file for mqtt

--- WOPR_Display/secret.h
+++ WOPR_Display/secret.h
@@ -1,2 +1,6 @@
 const char* ssid       = "PUT SSID HERE";
 const char* password   = "PUT PASSWORD HERE";
+const char* mqtt_server   = "mqtt server address/ip";
+const char* mqttclientID   = "mqtt client id";
+const char* mqttUser   = "mqtt user";
+const char* mqttPassword   = "mqtt password";
